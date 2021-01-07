dfb0424861b7b02f19e1c80404af9819f982409a
Leonardo Gonçalves de Oliveira <leonardo.g.o.lrv@gmail.com>
2021-01-07 13:43:26 -0400

    polimorfismo ex de uso em operacoes matematicas

--- br/com/xti/heranca/OperacaoTest.java
+++ br/com/xti/heranca/OperacaoTest.java
@@ -1,14 +1,12 @@
 package br.com.xti.heranca;
 
 public class OperacaoTest {
-
     public static void calcule(OperacaoMatematica o, double x, double y) {
-        o.calcular(x, y);
+        System.out.println(o.calcular(x, y));
     }
 
     public static void main(String[] args) {
-        calcule(new Soma(), 5, 5);
-        calcule(new Multiplicacao(), 5, 5);
+        calcule(new Soma(), 5.0, 5.0);
+        calcule(new Multiplicacao(), 5.0, 5.0);
     }
-
 }
