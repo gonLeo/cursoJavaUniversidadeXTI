ad611bf4e8467cc1e4f24e21100ce438c2ef2b2b
Leonardo Gonçalves de Oliveira <leonardo.g.o.lrv@gmail.com>
2021-01-12 09:55:28 -0400

    regex

--- br/com/xti/java/ExpressaoRegular.java
+++ br/com/xti/java/ExpressaoRegular.java
@@ -1,5 +1,8 @@
 package br.com.xti.java;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ExpressaoRegular {
     public static void main(String[] args) {
 
@@ -21,8 +24,40 @@ public class ExpressaoRegular {
         // \w letra [a-zA-Z_0-9]
         // \W não é letra
 
+        // QUANTIFICADORES
+        // X{n} X, exatamente n vezes
+        // X{n,} X, pelo menos n vezes
+        // X{n, m} X, pelo menos n vezes mas não mais do que m vezes
+        // X? X, 0 ou 1 vez
+        // X* X, 0 ou + 1 vezes
+        // X+ X, 1 ou + 1 vezes
+
+        // METACARACTERES DE FRONTEIRA
+        // ^ inicia
+        // $ finaliza
+        // | ou
+
+        // AGRUPADORES
+        // [...] Agrupamento
+        // [a-z] Alcance
+        // [a-e][i-u] União
+        // [a-z&&[aeiou]] Interseção
+        // [^abc] Exceção
+        // [a-z&&[^m-p]] Subtração
+        // \x Fuga literal
+
         b = "@".matches(".");
         b = "2".matches("\\d");
-        System.out.println(b);
+
+        String doce = "qual o DOCE mais doCe que o doce de batata Doce?";
+        Matcher matcher = Pattern.compile("(?i)doce").matcher(doce);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+
+        // SUBSTITUIÇÕES
+        String r = doce.replaceAll("(?i)doce", "docinho");
+        System.out.println(r);
+
     }
 }
